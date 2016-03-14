86b8673e91c1a688b8e700c56fba8f9307c5e099
hekme5 <emileissak@gmail.com>
2016-03-13 20:46:36 -0400

    First

---
+++ CreateCoverLetter/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CreateCoverLetter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.companyBox = new System.Windows.Forms.TextBox();
+            this.positionBox = new System.Windows.Forms.TextBox();
+            this.reqID = new System.Windows.Forms.TextBox();
+            this.location = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.experienceBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(47, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Company Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(47, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Position Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(47, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ReqID";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(52, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Location";
+            // 
+            // companyBox
+            // 
+            this.companyBox.Location = new System.Drawing.Point(409, 47);
+            this.companyBox.Name = "companyBox";
+            this.companyBox.Size = new System.Drawing.Size(454, 31);
+            this.companyBox.TabIndex = 4;
+            // 
+            // positionBox
+            // 
+            this.positionBox.Location = new System.Drawing.Point(409, 118);
+            this.positionBox.Name = "positionBox";
+            this.positionBox.Size = new System.Drawing.Size(454, 31);
+            this.positionBox.TabIndex = 5;
+            // 
+            // reqID
+            // 
+            this.reqID.Location = new System.Drawing.Point(409, 188);
+            this.reqID.Name = "reqID";
+            this.reqID.Size = new System.Drawing.Size(454, 31);
+            this.reqID.TabIndex = 6;
+            // 
+            // location
+            // 
+            this.location.Location = new System.Drawing.Point(409, 256);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(454, 31);
+            this.location.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(409, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(301, 55);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Get Cover Letter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // experienceBox
+            // 
+            this.experienceBox.Location = new System.Drawing.Point(409, 327);
+            this.experienceBox.Name = "experienceBox";
+            this.experienceBox.Size = new System.Drawing.Size(454, 31);
+            this.experienceBox.TabIndex = 9;
+            this.experienceBox.Text = "C#/ASP.NET/Java/JavaScript/SQL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label5.Location = new System.Drawing.Point(52, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 37);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Experience";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 497);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.experienceBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.location);
+            this.Controls.Add(this.reqID);
+            this.Controls.Add(this.positionBox);
+            this.Controls.Add(this.companyBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox companyBox;
+        private System.Windows.Forms.TextBox positionBox;
+        private System.Windows.Forms.TextBox reqID;
+        private System.Windows.Forms.TextBox location;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox experienceBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CreateCoverLetter/Form1.cs
@@ -0,0 +1,68 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateCoverLetter
+{
+    public partial class Form1 : Form
+    {
+        public string theCL = "Dear Madam/Sir,\n\n\nMy name is Emile Issaelkhoury and I would greatly appreciate to be considered for the {0} position (ReqID: {1}) at {2} in {3}. I believe that I would be a good fit for this position because my experience with C#/ASP.NET/Java/JavaScript/SQL, my current clearance, my B.S. degree in Computer engineer and my will to learn and to be challenged at {2}.\n\nI currently work as a software engineer with Novetta Solutions. I work on both the front end and the back end of a client web service. I develop backend code with C#, ASP.Net, SQL, AutoFac and nHibernate. I also work on the frontend using Javascript and multiple libraries like Backbone, jQuery and KnockoutJS. My current project is an agile project using JIRA and my position is going away because of funding. I currently hold an active TS clearance but held a TS/SCI and a CI/poly.\n\nPrior to this position, I was working on government site with BAE system as a backend developer using C#, .NET, JavaScript and SQL. I fixed bugs and update geocoder on the customer Google earth client: it parses a queried string (address) and provides latitude and longitude after multiple database lookup. The geocoder uses a RESTful web service that I manage through IIS. It is also available with downloadable libraries (DLLs).  I used the .NET to create a status page of the geocoder containing multiple charts and tables.\n\nEarlier, I was working with Lockheed Martin Corporation in Gaithersburg, MD as a Software engineer Asc. I was working on the IEC-ASP program under Rochelle Jones management. My main task was software integration of COTS into a government customer. This includes virtualization using Microsoft App-V. I also deployed these new packages using SCCM 2012. I worked with automated test software like Selenium and PhantomJS to test our widgets (OWF). This program used JIRA as an agile environment.\n\nI graduated from the University of Maryland Baltimore County (UMBC) in Baltimore, MD with a BS in Computer Engineering in May 2013 with a GPA of 3.31. Some of my relevant class work in engineering includes Principles of Digital Design, Circuits, Systems Design and Programing (assembly), Communication lab and Signals and Systems, DSP and Communication engineering. Most of my engineering classes required labs with MATLAB. I have also taken classes in computer science such as in programing (Python), Data structure (Java), Operating Systems (C language) and computer architecture (VHDL).\n\nBefore attending UMBC, I served in the United States Army working as a TMDE specialist (Test Measurements and Diagnostic Equipment specialist, MOS: 94H) or calibration specialist. My main responsibility was to test, calibrate and repair electronic test equipment used by the military. For over five years, I was responsible for maintaining a wide range of equipment that the military used to adjust their own equipment like torq wrench, oscilloscopes and RF (Radio Frequency) equipment; In summary, my daily duties was to fix hardware for the armed forces in the field. I was also trained as a Human Intelligence Specialist or HUMINT (MOS:35M) with the Army Reserves. I was in that position for two years. I am also a native speaker in Arabic and French including reading and writing.\n\nDuring the summer of 2012, I interned at the Northrop Grumman’s BWI facility in Linthicum, MD. I worked on testing failing RF parts, IMU testing and data mining. I also wrote different programs for engineers in MATLAB.\n\nIf you would like to meet in person or have any questions please feel free to reach me at (703)678-5980 (cell) or via email at emileissak@gmail.com.\n\nThank you for your time and consideration, I look forward to hearing back from you soon.\n\nSincerely,\nEmile Issaelkhoury";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string output = String.Format(theCL, positionBox.Text, reqID.Text, companyBox.Text,location.Text );
+            //System.IO.File.WriteAllText(@"C:\Users\emileMac\Desktop\CoverLetter_" + reqID.Text + ".txt", output);
+
+            System.IO.FileStream fs = new System.IO.FileStream(@"C:\Users\emileMac\Desktop\CoverLetter_" + reqID.Text + ".pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+
+            Document doc = new Document();
+
+
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+
+            doc.Open();
+
+            doc.Add(new Paragraph(output));
+
+
+            doc.Close();
+            System.Diagnostics.Process.Start(@"C:\Users\emileMac\Desktop\CoverLetter_" + reqID.Text + ".pdf");
+            Application.Exit();
+            
+        }
+    }
+}
